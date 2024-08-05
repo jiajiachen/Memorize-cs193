6d57fc81f0e1dab206966b7048d4577efd893763
chenjia595 <chenjia595@pingan.com.cn>
2024-08-05 21:54:26 +0800

    Initial Commit

---
+++ Project1/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  Project1
+//
+//  Created by Kelly Chen on 2024/8/5.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ Project1/Project1App.swift
@@ -0,0 +1,17 @@
+//
+//  Project1App.swift
+//  Project1
+//
+//  Created by Kelly Chen on 2024/8/5.
+//
+
+import SwiftUI
+
+@main
+struct Project1App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
