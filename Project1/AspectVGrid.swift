416addc0bc65017007aa6642ebb66c6f9eb59da1
KellyChen <matijueming@gmail.com>
2024-10-03 13:57:48 +0800

    CardView

---
+++ Project1/AspectVGrid.swift
@@ -0,0 +1,18 @@
+//
+//  AspectVGrid.swift
+//  Project1
+//
+//  Created by Kelly Chen on 2024/10/3.
+//
+
+import SwiftUI
+
+struct AspectVGrid: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    AspectVGrid()
+}
---
+++ Project1/CardView.swift
@@ -0,0 +1,18 @@
+//
+//  CardView.swift
+//  Project1
+//
+//  Created by Kelly Chen on 2024/10/3.
+//
+
+import SwiftUI
+
+struct CardView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    CardView()
+}
