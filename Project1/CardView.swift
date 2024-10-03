0700df1f0d68d5004259d5c4b656b1f4f17c9189
KellyChen <matijueming@gmail.com>
2024-10-03 13:58:37 +0800

    CardView2

--- Project1/CardView.swift
+++ Project1/CardView.swift
@@ -8,11 +8,33 @@
 import SwiftUI
 
 struct CardView: View {
+    let card: MemoryGame<String>.Card
+    init(_ card: MemoryGame<String>.Card) {
+        self.card = card
+    }
+    
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        ZStack {
+            let base = RoundedRectangle(cornerRadius: 12)
+            Group {
+                base.foregroundStyle(.white)
+                base.strokeBorder(lineWidth: 2)
+                Text(card.content)
+                    .font(.system(size: 200))
+                    .minimumScaleFactor(0.01)
+                    .aspectRatio(1, contentMode: .fit)
+            }
+            .opacity(card.isFaceUp ? 1 : 0) // 有很多种写法，这一句也可以不写，这一句与下一句有上下关系。
+            base.fill()
+                .opacity(card.isFaceUp ? 0 : 1) // 相当于mask 蒙板
+        }
+        .opacity(card.isFaceUp || !card.isMatched ? 1 : 0)
     }
 }
 
 #Preview {
-    CardView()
+    typealias Card = MemoryGame<String>.Card
+   return CardView(Card(content: "X", id: "test1"))
+        .padding()
+        .foregroundStyle(.green)
 }
